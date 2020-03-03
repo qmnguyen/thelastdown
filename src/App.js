f99ad042930a8788c1401aa18889ac0bde06bcb0
Quan Nguyen <quannguyen@QuanNguyensMBP2.fios-router.home>
2020-03-02 21:15:34 -0500

    Swapping out default React tut with login logic.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,38 @@
 import React from 'react';
 import './App.css';
-import Board from './Board';
+import Greeting from './Greeting';
+import { LoginButton, LogoutButton } from './Buttons';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
     this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      stepNumber: 0,
-      xIsNext: true,
-    }
+      isLoggedIn: false,
+    };
   }
 
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
+  handleLoginClick() {
     this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
+      isLoggedIn: true
+    })
   }
 
-  jumpTo(step) {
+  handleLogoutClick() {
     this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    });
+      isLoggedIn: false
+    })
   }
 
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
 
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
+    if(isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      button = <LoginButton onClick={this.handleLoginClick} />
     }
 
     return (
@@ -68,17 +42,9 @@ class App extends React.Component {
             The Last Down
           </p>
         </header>
-        <div className="game">
-          <div className="game-board">
-            <Board
-              squares={current.squares}
-              onClick={(i) => this.handleClick(i)}
-            />
-          </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol>{moves}</ol>
-          </div>
+        <div className="TLD-greeting">
+          <Greeting isLoggedIn={isLoggedIn} />
+          { button }
         </div>
       </div>
     );
@@ -87,22 +53,4 @@ class App extends React.Component {
 
 export default App;
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+// You can define helper functions at the end here.
---
+++ src/Buttons.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>Login</button>
+  )
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>Logout</button>
+  )
+}
+
+export { LoginButton, LogoutButton };
